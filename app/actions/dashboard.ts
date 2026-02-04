f0c90f568203f69e6da94cafb0f5af61072817aa
7Francus7 <dellorsif@gmail.com>
2026-02-04 14:40:43 -0300

    fix: typo in dashboard actions

--- app/actions/dashboard.ts
+++ app/actions/dashboard.ts
@@ -15,7 +15,7 @@ export async function getDashboardStats() {
                      salesToday,
                      productsCount,
                      lowStockCount,
-                    rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr customersCount
+                     customersCount
               ] = await Promise.all([
                      // Sales Today
                      prisma.sale.aggregate({
