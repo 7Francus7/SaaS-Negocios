77be3974eb49108fd0a75893b2d415e507a3283b
7Francus7 <dellorsif@gmail.com>
2026-03-04 23:28:22 -0300

    chore: actualiza script de prueba de prisma

--- test-prisma.js
+++ test-prisma.js
@@ -2,14 +2,8 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 async function main() {
        try {
-              const res = await prisma.customer.update({
-                     where: { id: 1 },
-                     data: {
-                            currentBalance: { decrement: -400 },
-                            closedBalance: { decrement: 0 }
-                     }
-              });
-              console.log("Success", res);
+              const user = await prisma.user.findUnique({ where: { email: 'despensafran@saas.com' } });
+              console.log("User:", user);
        } catch (e) {
               console.error("Error is", e);
        } finally {
